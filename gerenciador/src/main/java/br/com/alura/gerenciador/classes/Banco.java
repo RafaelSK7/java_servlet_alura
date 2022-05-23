3d7fd9ae9daa8d1a9b4551b22d3c56dd7bfd591c
Rafael Kiss <rafaelskiss1@hotmail.com>
2022-05-23 16:41:59 -0300

    Formulário de alteração

--- gerenciador/src/main/java/br/com/alura/gerenciador/classes/Banco.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/classes/Banco.java
@@ -46,4 +46,13 @@ public class Banco {
 			}
 		}
 	}
+
+	public Empresa buscaEmpresaId(Integer id) {
+		for(Empresa empresa : lista) {
+			if(empresa.getId() == id)
+				return empresa;
+		}
+		
+		return null;
+	}
 }
